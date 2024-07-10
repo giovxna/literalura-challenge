d75e1d32b4d29b24f171de1eafe832348a96150b
giovxna <gi.siqueira0202@gmail.com>
2024-07-10 17:13:15 -0300

    initial commit

---
+++ src/test/java/giovxna/literalura/LiteraluraApplicationTests.java
@@ -0,0 +1,13 @@
+package giovxna.literalura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
